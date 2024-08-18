5e6950fe444e7f13214858adeca86b6e68c70f34
Jackson Dorsett <dorsettj@gmail.com>
2024-08-18 14:48:18 -0700

    added interfaces

--- src/interfaces/OauthAccount.interface.ts
+++ src/interfaces/OauthAccount.interface.ts
@@ -6,6 +6,7 @@ interface IOauthAccount {
   access_token: string;
   refresh_token: string;
   token_expires_at: Date;
+  refresh_token_expires_at: Date;
   created_at: Date;
   updated_at: Date;
 }
