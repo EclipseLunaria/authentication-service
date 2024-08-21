1a4f343cc348133f4d43c7d4c8eaf8404e5fdcf5
Jackson Dorsett <dorsettj@gmail.com>
2024-08-20 19:27:39 -0700

    fixed prod config

--- pm2.config.js
+++ pm2.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: "authenticator",
       script: "node_modules/.bin/ts-node",
-      args: "./server.ts",
-      cwd: "./src",
+      args: "./src/server.ts",
+      cwd: "./",
       watch: true, // Enable watch mode
       ignore_watch: ["node_modules"],
       env: {
